584f9bf8fcecd06e8abd5f08fdf3c9a8d293713e
Krystian <lis.krystian.10@gmail.com>
2020-01-03 14:12:05 +0100

    add displaying mathematical operation to solve

--- BravoGame/Source/Engine/GamePlay/World/UI.cs
+++ BravoGame/Source/Engine/GamePlay/World/UI.cs
@@ -25,6 +25,7 @@ namespace BravoGame
             string tempString = $"Points = {world.Points}";
             Vector2 stringDimensions = font.MeasureString(tempString);
             Globals.spriteBatch.DrawString(font, tempString, new Vector2(Globals.ScreenWidth / 2 - stringDimensions.X / 2, Globals.ScreenHeight - stringDimensions.Y), Color.Black);
+            
             HealthBar.Draw(new Vector2(20, Globals.ScreenHeight - 40));
         }
     }
